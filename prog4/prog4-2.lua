37987ddeecf6bccd5894ec55241f0fefa6c06402
brianchenoweth <brianmchenoweth@gmail.com>
2016-10-25 20:47:53 -0700

    Create prog4-2.lua

---
+++ prog4/prog4-2.lua
@@ -0,0 +1,12 @@
+print("Assignment #4-2, Brian Chenoweth, masc1367")
+for i = 1, 100 do
+   if i % 3 == 0 and i % 5 == 0 then
+      print "FizzBuzz"
+   elseif i % 3 == 0 then
+      print "Fizz"
+   elseif i % 5 == 0 then
+      print "Buzz"
+   else
+      print(i)
+   end
+end

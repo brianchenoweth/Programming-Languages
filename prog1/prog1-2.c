e197cb0d07a9ba1c667ab232f344f96418d67479
brianchenoweth <brianmchenoweth@gmail.com>
2016-10-25 20:19:37 -0700

    Create prog1-2.c

---
+++ prog1/prog1-2.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+int main ()
+{
+	
+	int name;
+	
+	printf("Assignment #1-2, Brian Chenoweth, masc1367\n");
+	printf("Hello! What is your name?\n");
+	scanf("%s", name);
+	printf("Hello %s. Nice to meet you!\n", name);
+	return 0;
+	
+	}
+	

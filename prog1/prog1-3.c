5242adb02dda90730262913d53e7dc048975f83e
brianchenoweth <brianmchenoweth@gmail.com>
2016-10-25 20:20:06 -0700

    Create prog1-3.c

---
+++ prog1/prog1-3.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<math.h>
+#define PI 3.14159265359
+
+int main ()
+{
+	
+	int num;
+	
+	printf("Assignment #1-3, Brian Chenoweth, masc1367\n");
+	printf("Please input an integer:\n");
+	scanf("%d",&num);
+	printf("%.3f\n",(sin(M_PI*num/180)));
+	return 0;
+	
+	}
+	
+	
+	
